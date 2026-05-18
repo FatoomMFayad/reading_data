41b1a89b7a50e98be140e64e0d6a4666f0146a93
ffayad <ffayad@ptcdb.edu.ps>
2026-05-18 15:31:57 +0300

    claening researchers.csv file

--- cleaning_data.py
+++ cleaning_data.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
 df = pd.read_csv('data/researchers.csv')
-print(df.shape)
-print(df.head())
+# print(df.shape)
+# print(df.head())
+
+df = df.dropna()
+df = df.drop_duplicates()
+
+df = df[(df['h_index'] > 15) & 
+(df['is_active']== True)].sort_values('joined_year', ascending=True)
+print(*df['last_name'].str[0], sep=' ')
